0d92de83887dea9dcab8fa2e007892b72b8abd1a
byron mccormick <byronjamesmccormick@gmail.com>
2024-08-08 15:06:22 +1200

    add view order features

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -7,6 +7,7 @@ import ProtectedRoute from "./auth/ProtectedRoute"
 import ManageRestaurantPage from "./pages/ManageRestaurantPage"
 import SearchPage from "./pages/SearchPage"
 import DetailPage from "./pages/DetailPage"
+import OrderStatusPage from "./pages/OrderStatusPage"
 
 const AppRoutes = ()=> {
     return(
@@ -17,6 +18,14 @@ const AppRoutes = ()=> {
             <Route path="/detail/:restaurantId" element={<Layout showHero={false}><DetailPage /></Layout>}> </Route>
             
             <Route element={<ProtectedRoute/>}>
+                <Route 
+                    path="/order-status"
+                    element={
+                    <Layout>
+                        <OrderStatusPage/>
+                    </Layout>
+                }>
+                </Route>
                 <Route 
                     path="/user-profile" 
                     element={
