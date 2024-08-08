db10b6d37d9bac6690fc6370205563dc243c3d50
byron mccormick <byronjamesmccormick@gmail.com>
2024-08-08 20:05:54 +1200

    added logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,12 @@ import MainNav from "./MainNav";
 
 const Header = () => {
   return (
-    <div className="border-b-2 border-b-orange-500 py-6">
+    <div className="border-b-2 border-b-orange-500 py-4">
         <div className="container mx-auto flex justify-between items-center">
-            <Link className="text-3xl font-bold tracking-tight text-orange-500" to="/">āeCHEF</Link>
+            <Link className="flex justify-center items-center text-3xl font-bold tracking-tight text-orange-500" to="/">
+            <img className="inline w-16 mr-2 mb-2" src="../../apple-touch-icon.png"/>
+            āeCHEF
+            </Link>
             <div className="md:hidden">
                 <MobileNav />
             </div>
